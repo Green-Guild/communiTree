e11df8a8d1189181d91541578da91bfd492e30ff
daniel-figg <daniel.figg05@gmail.com>
2024-05-07 12:36:04 -0400

    fix(env): typo

--- server/strategies/google-strategy.js
+++ server/strategies/google-strategy.js
@@ -1,11 +1,13 @@
 import passport from 'passport';
 import { Strategy } from 'passport-google-oauth20';
 import User from '../db/models/User.js';
+import dotenv from 'dotenv'
+dotenv.config()
 
 export default passport.use(
   new Strategy(
     {
-      clientID: process.env.GOOGLE_ClIENT_ID,
+      clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/api/google/callback',
       scope: ['profile'],
