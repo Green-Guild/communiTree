9d734bafc6d8cfde3257617479dd3a9aa147821d
daniel-figg <daniel.figg05@gmail.com>
2024-05-07 14:32:34 -0400

    fix: convert to es6

--- server/db/migrations/20240506164148_createTables.js
+++ server/db/migrations/20240506164148_createTables.js
@@ -2,55 +2,53 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+export function up(knex) {
   return knex.schema
-  .createTable('posts',(table) => {
-    table.increments();
-    table.string('title').notNullable();
-    table.string('body').notNullable();
-    table.integer('user_id').notNullable();
-    table.integer('garden_id').nullable();
-    table.integer('events_id').nullable();
-    table.timestamps(true, true)
-  })
-  .createTable('gardens',(table) => {
-    table.increments();
-    table.string('name').notNullable();
-    table.string('location').notNullable();
-    table.string('image').notNullable();
-    table.string('description').notNullable();
-    table.boolean('public').notNullable()
-    table.integer('owner_id').nullable();
-  })
-  .createTable('comments',(table) => {
-    table.increments();
-    table.integer('post_id').notNullable();
-    table.integer('user_id').notNullable();
-    table.string('body').notNullable();
-    table.timestamp(true,true)
-  })
-  .createTable('events',(table) => {
-    table.increments();
-    table.string('location').notNullable();
-    table.string('description').notNullable();
-    table.integer('host_id').notNullable();
-    table.integer('garden_id').nullable()
-    table.dateTime('date').notNullable()
-    table.text('image').nullable()
-    table.text('title').notNullable()
-  })
-  
-};
+    .createTable('posts', (table) => {
+      table.increments();
+      table.string('title').notNullable();
+      table.string('body').notNullable();
+      table.integer('user_id').notNullable();
+      table.integer('garden_id').nullable();
+      table.integer('events_id').nullable();
+      table.timestamps(true, true);
+    })
+    .createTable('gardens', (table) => {
+      table.increments();
+      table.string('name').notNullable();
+      table.string('location').notNullable();
+      table.string('image').notNullable();
+      table.string('description').notNullable();
+      table.boolean('public').notNullable();
+      table.integer('owner_id').nullable();
+    })
+    .createTable('comments', (table) => {
+      table.increments();
+      table.integer('post_id').notNullable();
+      table.integer('user_id').notNullable();
+      table.string('body').notNullable();
+      table.timestamp(true, true);
+    })
+    .createTable('events', (table) => {
+      table.increments();
+      table.string('location').notNullable();
+      table.string('description').notNullable();
+      table.integer('host_id').notNullable();
+      table.integer('garden_id').nullable();
+      table.dateTime('date').notNullable();
+      table.text('image').nullable();
+      table.text('title').notNullable();
+    });
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+export function down(knex) {
   return knex.schema
     .dropTable('posts')
     .dropTable('gardens')
     .dropTable('comments')
-    .dropTable('events')
-  
-};
+    .dropTable('events');
+}
