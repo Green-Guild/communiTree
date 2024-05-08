2772d5564eacd55a8fc16c12ce28dd5c48d8c1a3
angelicacc00 <angelicac0721@gmail.com>
2024-05-08 14:24:59 -0400

    files created

---
+++ frontend/src/pages/Gardens.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import fetchData from '../utils/fetchData';
+
+const Gardens = ({city}) => {
+
+  return (
+  <main>
+    <div>
+    </div>
+  </main>
+  );
+};
+export default Gardens;
