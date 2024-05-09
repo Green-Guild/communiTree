5dbbdeae7c94fb379f1ac684695d72c6abdcc6a5
daniel-figg <daniel.figg05@gmail.com>
2024-05-08 21:20:06 -0400

    feat(auth): update adapters

--- frontend/src/adapters/auth-adapter.js
+++ frontend/src/adapters/auth-adapter.js
@@ -1,4 +1,4 @@
-import { fetchHandler, getPostOptions, deleteOptions } from "../utils";
+import { fetchHandler, getPostOptions, deleteOptions } from '../utils';
 
 const baseUrl = '/api';
 
@@ -7,12 +7,15 @@ export const checkForLoggedInUser = async () => {
   return data;
 };
 
-export const logUserIn = async ({ username, password }) => (
-  fetchHandler(`${baseUrl}/login`, getPostOptions({ username, password }))
-);
+export const localLogin = async ({ username, password }) =>
+  fetchHandler(`${baseUrl}/login`, getPostOptions({ username, password }));
+
+export const googleLogin = () => {
+  window.location.href = `${baseUrl}/google/`;
+};
 
 // the logout route pretty much can't fail with our setup, but if yours can, change this
-export const logUserOut = async () => {
+export const logout = async () => {
   await fetchHandler(`${baseUrl}/logout`, deleteOptions);
   return true;
 };
