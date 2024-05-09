3e2417c38d3e6979e10a6dd1e1508b7ba3ca501a
daniel-figg <daniel.figg05@gmail.com>
2024-05-09 11:15:45 -0400

    feat(posts): setup adapter

---
+++ frontend/src/adapters/post-adapter.js
@@ -0,0 +1,54 @@
+import {
+  fetchHandler,
+  getPatchOptions,
+  getPostOptions,
+  deleteOptions,
+} from '../utils';
+
+const baseUrl = '/api/posts';
+
+export const createPost = async ({ title, body, garden_id, event_id }) => {
+  const [data, err] = await fetchHandler(
+    baseUrl,
+    getPostOptions({
+      title,
+      body,
+      garden_id,
+      event_id,
+    })
+  );
+  if (err) return err;
+  return data;
+};
+
+export const getAllPosts = async () => {
+  const [data, err] = await fetchHandler(baseUrl);
+  if (err) return err;
+  return data;
+};
+
+export const getPost = async (id) => {
+  const [data, err] = await fetchHandler(`${baseUrl}/${id}`);
+  if (err) return err;
+  return data;
+};
+
+export const updatePost = async ({ title, body, garden_id, event_id }) => {
+  const [data, err] = await fetchHandler(
+    `${baseUrl}/${id}`,
+    getPatchOptions({
+      title,
+      body,
+      garden_id,
+      event_id,
+    })
+  );
+  if (err) return err;
+  return data;
+};
+
+export const deletePost = async (id) => {
+  const [data, err] = await fetchHandler(`${baseUrl}/${id}`, deleteOptions);
+  if (err) return err;
+  return data;
+};
