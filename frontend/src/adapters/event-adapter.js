316588e0e9c828fd9727ddc569d06ababc888c5b
daniel-figg <daniel.figg05@gmail.com>
2024-05-09 11:00:56 -0400

    feat(events): setup adapters

---
+++ frontend/src/adapters/event-adapter.js
@@ -0,0 +1,72 @@
+import {
+  fetchHandler,
+  getPatchOptions,
+  getPostOptions,
+  deleteOptions,
+} from '../utils';
+
+const baseUrl = '/api/events';
+
+export const createEvent = async ({
+  title,
+  description,
+  location,
+  event_date,
+  garden_id,
+  image,
+}) => {
+  const [data, err] = await fetchHandler(
+    baseUrl,
+    getPostOptions({
+      title,
+      description,
+      location,
+      event_date,
+      garden_id,
+      image,
+    })
+  );
+  if (err) return err;
+  return data;
+};
+
+export const getAllEvents = async () => {
+  const [data, err] = await fetchHandler(baseUrl);
+  if (err) return err;
+  return data;
+};
+
+export const getEvent = async (id) => {
+  const [data, err] = await fetchHandler(`${baseUrl}/${id}`);
+  if (err) return err;
+  return data;
+};
+
+export const updateEvent = async ({
+  title,
+  description,
+  location,
+  event_date,
+  garden_id,
+  image,
+}) => {
+  const [data, err] = await fetchHandler(
+    `${baseUrl}/${id}`,
+    getPatchOptions({
+      title,
+      description,
+      location,
+      event_date,
+      garden_id,
+      image,
+    })
+  );
+  if (err) return err;
+  return data;
+};
+
+export const deleteEvent = async (id) => {
+  const [data, err] = await fetchHandler(`${baseUrl}/${id}`, deleteOptions);
+  if (err) return err;
+  return data;
+};
