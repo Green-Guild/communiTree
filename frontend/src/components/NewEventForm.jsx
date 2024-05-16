cb72c126b9f65fce495727fb0c05ccf18640fa67
angelicacc00 <angelicac0721@gmail.com>
2024-05-16 16:35:50 -0400

    small updates

--- frontend/src/components/NewEventForm.jsx
+++ frontend/src/components/NewEventForm.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { createEvent } from '../adapters/event-adapter';
+
 const NewEventForm = () => {
 
   const [showForm, setShowForm] = useState(false);
@@ -42,16 +44,10 @@ const NewEventForm = () => {
               />
             </label>
           </div>
-          <label>Date and Time:
+          <div>
+            <label>Date and Time:
               <input 
                 type='datetime'
-                required
-              />
-            </label>
-          <div>
-            <label>Public:
-              <input 
-                type="checkbox"
                 required
               />
             </label>
@@ -63,70 +59,3 @@ const NewEventForm = () => {
   </>
 }
 export default NewEventForm
-
-/*
-import { useState } from 'react';
-import { createGarden } from '../adapters/garden-adapter';
-
-const NewGardenForm = ({ownerId}) => {
-
-  const [showForm, setShowForm] = useState(false);
-
-  const toggleForm = () => {
-    setShowForm(prevState => !prevState);
-  }
-  
-
-  return <>
-  <div>
-    <button onClick={toggleForm}>Start a Garden</button>
-      {showForm && (
-        <form>
-          <div>
-            <label>Name:
-              <input 
-                type="text"
-                required
-              />
-            </label>
-          </div>
-          <div>
-            <label>Location (Zipcode):
-              <input 
-                type="text"
-                required
-              />
-            </label>
-          </div>
-          <div>
-            <label>Description:
-              <textarea
-                required
-              ></textarea>
-            </label>
-          </div>
-          <div>
-            <label htmlFor="image">Image URL:
-              <input 
-                type="url"
-                required
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="is_public">Public:
-              <input 
-                type="checkbox"
-                required
-              />
-            </label>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      )}
-      </div>
-    </>
-};
-
-export default NewGardenForm;
-*/
